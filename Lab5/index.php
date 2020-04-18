dcfee169eb65cbf44a3f56c896cc5c1adf2cece9
kolaqsq <kolaqsq1707@gmail.com>
2020-04-18 17:15:02 +0300

    Started Lab5

---
+++ Lab5/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/fonts.css">
+    <title>Огородников Николай Александрович, 191-321.</title>
+</head>
+<body>
+<header>
+    <div class="logo">
+        <img src="img/logo.png" alt="logo">
+    </div>
+    <div class="title">
+        <h2>Огородников Николай Александрович, 191-321</h2>
+        <h1>Лабораторная работа какая-то, вариант именно тот</h1>
+    </div>
+</header>
+<main></main>
+<footer></footer>
+</body>
+</html>
