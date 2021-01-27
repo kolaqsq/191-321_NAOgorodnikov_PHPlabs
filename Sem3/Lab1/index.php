c08ce3397c7d27f81a483ef8fd50410e2c56cd60
kolaqsq <kolaqsq1707@gmail.com>
2021-01-27 23:20:36 +0300

    tree works now

--- Sem3/Lab1/index.php
+++ Sem3/Lab1/index.php
@@ -10,12 +10,12 @@ if (isset($_GET['logout'])) {
 if (!isset($_SESSION['user']) && isset($_POST['login']) &&
     isset($_POST['password']) && $f = fopen('users.csv', 'rt')) {
     while (!feof($f)) {
-        $test_user = explode(';', fgets($f));
+        $auth_user = explode(';', fgets($f));
 
-        if (trim($test_user[0]) == $_POST['login']) {
-            if (isset($test_user[1]) &&
-                trim($test_user[1]) == $_POST['password']) {
-                $_SESSION['user'] = $test_user;
+        if (trim($auth_user[0]) == $_POST['login']) {
+            if (isset($auth_user[1]) &&
+                trim($auth_user[1]) == $_POST['password']) {
+                $_SESSION['user'] = $auth_user;
                 header('Location: index.php');
                 exit();
             } else
@@ -34,6 +34,7 @@ if (!isset($_SESSION['user']) && isset($_POST['login']) &&
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/main.css">
     <title>Login Page</title>
 </head>
 <body>
--- Sem3/Lab1/tree.php
+++ Sem3/Lab1/tree.php
@@ -1,15 +1,32 @@
 <?php
-//session_start();
-//
-//if (isset($_GET['logout'])) {
-//    unset($_SESSION['user']);
-//    header('Location: index.php');
-//    exit();
-//}
-//
-//echo '<a href="tree.php?logout=">Выход</a>';
+function outputDirInfo($name, $path)
+{
+    echo '<div class="dir-tree">Каталог ' . $name . '<br>';
+    $dir = opendir($path);
+    while (($file = readdir($dir)) !== false) {
+        if ($file != "." && $file != "..") {
+            if (is_dir($path . '\\' . $file)) {
+//                echo "$file, $path\\$file";
+                outputDirInfo($file, $path . '\\' . $file);
+            }
 
-//echo '<p>Добро пожаловать, ' . $_SESSION['user']['name'] . '!</p>';
+            else
+                if (is_file($path . '\\' . $file))
+                    makeLink($file, $path);
+        }
+    }
+    closedir($dir);
+    echo '</div>';
+}
 
+function makeLink($name, $path)
+{
+    echo '<a class="dir-tree" href="viewer.php?filename=' . UrlEncode($path) .
+        '/' . $name . '">Файл ' . $name . '</a>';
+}
+
+echo '<div>';
+outputDirInfo('корневой', getcwd());
+echo '</div>';
 
 echo '<p>tree works</p>';
